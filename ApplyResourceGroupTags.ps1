2b7679a7bb5fc68aa267919a0fb142c3dd0ca256
quinnmeagher <Quinn.Meagher@microsoft.com>
2023-08-01 13:09:16 -0400

    update error handling

--- ApplyResourceGroupTags.ps1
+++ ApplyResourceGroupTags.ps1
@@ -1,21 +1,34 @@
-﻿# Login to Azure Powershell CLI
-Connect-AzAccount -TenantId <YOUR_TENANT_ID>
+﻿##ApplyResourceGroupTags.ps1
+
+try {
+    # Login to Azure Powershell CLI
+    Connect-AzAccount -TenantId "{{TENANT_ID}}"
+} catch {
+    # Log auth exceptions
+    Write-Host $_ -ForegroundColor Black -BackgroundColor white
+}
 
 # Parse CSV for String Content and strip header valuers
 $csvTagData = Import-Csv -Path .\TagData.csv
 
 # Iterate over rows
 foreach ($row in $csvTagData){
-
     # Set Subscription Context
     $subscriptionContext = Set-AzContext -Subscription $row.SubscriptionName
-
-    # Query for ResourceGroup ResourceId
-    $rg = Get-AzResourceGroup -Name $row.ResourceGroup
     
+    # Create Tags dictionary
     $tags = @{"Owner"=$row.Owner; "Department"=$row.Department; "Environment"=$row.Environment; "CostCenter"=$row.CostCenter}
+    try {
+        # Query for ResourceGroup ResourceId
+        $rg = Get-AzResourceGroup -Name $row.ResourceGroup
 
-    # Apply Tags
-    Write-Host "Processing "$rg.ResourceGroupName"" -ForegroundColor Black -BackgroundColor white
-    Update-AzTag -ResourceId $rg.ResourceId -Tag $tags -operation Merge
+        # Apply Tags
+        Write-Host "Processing "$rg.ResourceGroupName"" -ForegroundColor Black -BackgroundColor white
+
+        # Update Tags for specific ResourceGroup
+        $updatedTag = Update-AzTag -ResourceId $rg.ResourceId -Tag $tags -operation Merge
+    } catch {
+        # Log exceptions
+        Write-Host $_ -ForegroundColor Black -BackgroundColor white
+    }
 } 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AzureTaggingPowershellSamples
+
+.\ApplyResourceGroupTags.ps1 2>&1 | Tee-Object -FilePath results.txt
